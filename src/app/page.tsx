c8529f0046542e5a0e71ed2421aa421b8d897065
0xyshv <writetoyshv@gmail.com>
2024-10-11 12:05:26 +0530

    chore: add landing UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,74 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <main className="flex flex-col gap-8 items-center px-24 pt-12">
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-bold text-3xl lg:flex">
+          <p className="font-sans italic text-4xl text-black">
+            VisualizeIt
+            <span className="font-bold text-blue-500">.ai</span>
+          </p>
+          <div className="fixed bottom-0 left-0 flex w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+            <Link href="/">
+              <Button className="font-bold text-xl">Launch App</Button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-col justify-center gap-4 items-center mt-24">
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex items-center gap-2 text-4xl font-bold text-primary ">
+              Welcome to <p className="font-sans italic text-4xl text-black">
+                VisualizeIt
+                <span className="font-bold text-blue-500">.ai</span>
+              </p>
+            </div>
+            <p className="text-muted-foreground">
+              A tool for creators and story tellers to create visuals for their narrration automatically using AI.
+            </p>
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className="grid grid-cols-3 gap-8 px-20 mt-10">
+          <Card className="shadow-sm border-none h-full w-full rounded-lg bg-yellow-200 bg-opacity-15 border cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
+            <CardHeader>
+              <CardDescription className="text-center pt-1 text-lg text-black">
+                This is a test feature of VisualizeIt.ai
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card className="shadow-sm border-none h-full w-full rounded-lg bg-yellow-200 bg-opacity-15 border cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
+            <CardHeader>
+              <CardDescription className="text-center pt-1 text-lg text-black">
+                This is a test feature of VisualizeIt.ai
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card className="shadow-sm border-none h-full w-full rounded-lg bg-yellow-200 bg-opacity-15 border cursor-pointer transform transition-transform duration-300 ease-in-out hover:scale-110">
+            <CardHeader>
+              <CardDescription className="text-center pt-1 text-lg text-black">
+                This is a test feature of VisualizeIt.ai
+              </CardDescription>
+            </CardHeader>
+          </Card>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="fixed container bottom-4 w-full">
+        <div className="flex justify-center items-center w-full ml-10">
+          <p className="text-muted-foreground">
+            &copy;{" "}
+            <a href="https://www.alphadevs.dev/" target="_blank">
+              alphadevs.dev
+            </a>{" "}
+          </p>
+        </div>
       </footer>
-    </div>
-  );
+    </>
+  )
 }
