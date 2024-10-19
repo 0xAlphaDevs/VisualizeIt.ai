1e2352085fee78c11348ab21fb172bd84af9e753
0xyshv <writetoyshv@gmail.com>
2024-10-19 18:15:59 +0530

    chore: sync

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ import React from 'react'
 const Navbar = () => {
   const pathname = usePathname();
   return (
-    <div className='mx-16 my-6 px-8 py-2 flex items-center gap-24 shadow-md border border-blue-200 bg-blue-400 bg-opacity-15 rounded-full '>
-      <Link href="/dashboard" className="font-sans italic text-4xl text-black">
+    <div className='mx-28 my-6 px-8 py-2 flex items-center gap-24 shadow-md border border-blue-200 bg-blue-400 bg-opacity-15 rounded-full '>
+      <Link href="/dashboard" className="font-sans font-bold italic text-2xl text-black">
         VisualizeIt
         <span className="font-bold text-blue-500">.ai</span>
       </Link>
