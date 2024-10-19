83853ca1eac6085076d30a32468d4ce79d02074f
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-10-19 10:22:48 +0530

    feat: add zora sdk

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import VideoGenerator from "@/components/video-generator";
-import { ConnectKitButton } from "connectkit";
-import React from "react";
+// import { ConnectKitButton } from "connectkit";
+// import React from "react";
 
 const CreatePage = () => {
   return (
     <div>
-      <ConnectKitButton />
+      {/* <ConnectKitButton /> */}
       <VideoGenerator />
     </div>
   );
